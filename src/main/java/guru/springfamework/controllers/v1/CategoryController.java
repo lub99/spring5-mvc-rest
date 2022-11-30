21204b3577e4322cc0aa3b2fd8bef342558df6ce
lubwizard <matej.luburic@stack-wizard.com>
2022-11-30 10:48:45 +0100

    Add category controller with tests.

---
+++ src/main/java/guru/springfamework/controllers/v1/CategoryController.java
@@ -0,0 +1,34 @@
+package guru.springfamework.controllers.v1;
+
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.api.v1.model.CategoryListDto;
+import guru.springfamework.services.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping({"", "/"})
+    public ResponseEntity<CategoryListDto> getCategories() {
+        return ResponseEntity.ok(
+                new CategoryListDto(categoryService.getAllCategories())
+        );
+    }
+
+    @GetMapping( "/{name}")
+    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
+        return ResponseEntity.ok(
+                categoryService.getCategoryByName(name)
+        );
+    }
+}
